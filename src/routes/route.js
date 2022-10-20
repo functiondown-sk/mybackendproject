312de388f3a558fd68e2035e8c4820578624324b
vishnus25 <vishnulnct8235@gmail.com>
2022-10-20 23:42:22 +0530

    query params assignment 20/10/22

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,45 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+// ASSIGNMENT:
+// you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+// ================Answer=====================//
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+
+ router.post("/validage" , function(req,res) {
+    const voting = req.query.age
+    const newarr = []
+    for (let index = 0; index <persons.length; index++ ){
+        let element = persons[index];
+        if((element.age>= voting)){
+            element.votingStatus = true
+            newarr.push(element)
+        }
+    }res.send({final: newarr})
+    console.log(persons)
+ })
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
 
 
 
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
 
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 router.post("/test-post", function(req, res) {
     res.send([ 23, 45 , 6])
